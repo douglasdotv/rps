e21f34e02fc93170b2ed60d62898fb98d6e9b74c
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-08 15:46:41 -0300

    Implement stage 1: "unfair computer"

--- src/main/java/br/com/dv/rockpaperscissors/Main.java
+++ src/main/java/br/com/dv/rockpaperscissors/Main.java
@@ -1,9 +1,22 @@
 package br.com.dv.rockpaperscissors;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String userInput = scanner.nextLine();
+
+        String computerMove = switch (userInput) {
+            case "paper" -> "scissors";
+            case "scissors" -> "rock";
+            case "rock" -> "paper";
+            default -> null;
+        };
+
+        System.out.printf("Sorry, but the computer chose %s", computerMove);
     }
 
 }
