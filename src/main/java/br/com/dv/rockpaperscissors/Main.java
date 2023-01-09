461786a77b9f0fa4902a4b3e17a69084b6749515
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-09 09:36:02 -0300

    Implement stage 3: "endless game"

--- src/main/java/br/com/dv/rockpaperscissors/Main.java
+++ src/main/java/br/com/dv/rockpaperscissors/Main.java
@@ -9,41 +9,59 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String playerMove = scanner.nextLine();
+        Random random = new Random();
 
         List<String> potentialMoves = List.of("rock", "scissors", "paper");
 
-        Random random = new Random();
-        int randomIndex = random.nextInt(potentialMoves.size());
-        String computerMove = potentialMoves.get(randomIndex);
+        while (true) {
+            String userInput = scanner.nextLine();
 
-        if (Objects.equals(playerMove, computerMove)) {
-            System.out.printf("There is a draw (%s)", playerMove);
-        } else {
-            switch (playerMove) {
-                case "rock":
-                    if (Objects.equals(computerMove, "paper")) {
-                        System.out.printf("Sorry, but the computer chose %s", computerMove);
-                    } else {
-                        System.out.printf("Well done. The computer chose %s and failed", computerMove);
-                    }
+            if (Objects.equals(userInput, "!exit")) {
+                System.out.print("Bye!");
+                break;
+            }
+
+            boolean isMove = false;
+            for (String move : potentialMoves) {
+                if (Objects.equals(userInput, move)) {
+                    isMove = true;
                     break;
-                case "paper":
-                    if (Objects.equals(computerMove, "scissors")) {
-                        System.out.printf("Sorry, but the computer chose %s", computerMove);
-                    } else {
-                        System.out.printf("Well done. The computer chose %s and failed", computerMove);
+                }
+            }
+
+            if (!isMove) {
+                System.out.println("Invalid input");
+            } else {
+                int randomIndex = random.nextInt(potentialMoves.size());
+                String computerMove = potentialMoves.get(randomIndex);
+
+                if (Objects.equals(userInput, computerMove)) {
+                    System.out.printf("There is a draw (%s)%n", userInput);
+                } else {
+                    switch (userInput) {
+                        case "rock":
+                            if (Objects.equals(computerMove, "paper")) {
+                                System.out.printf("Sorry, but the computer chose %s%n", computerMove);
+                            } else {
+                                System.out.printf("Well done. The computer chose %s and failed%n", computerMove);
+                            }
+                            break;
+                        case "paper":
+                            if (Objects.equals(computerMove, "scissors")) {
+                                System.out.printf("Sorry, but the computer chose %s%n", computerMove);
+                            } else {
+                                System.out.printf("Well done. The computer chose %s and failed%n", computerMove);
+                            }
+                            break;
+                        case "scissors":
+                            if (Objects.equals(computerMove, "rock")) {
+                                System.out.printf("Sorry, but the computer chose %s%n", computerMove);
+                            } else {
+                                System.out.printf("Well done. The computer chose %s and failed%n", computerMove);
+                            }
+                            break;
                     }
-                    break;
-                case "scissors":
-                    if (Objects.equals(computerMove, "rock")) {
-                        System.out.printf("Sorry, but the computer chose %s", computerMove);
-                    } else {
-                        System.out.printf("Well done. The computer chose %s and failed", computerMove);
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid player move.");
+                }
             }
         }
     }
